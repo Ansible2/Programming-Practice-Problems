3894964dddbf84e7cc21f17ddb7dc2ab5bc35d94
Ansible2 <afoshark@yahoo.com>
2021-07-27 08:14:43 -0600

    Finished Counting Valleys Problem in both C++ and C#

---
+++ HackerRank Problems/Easy/C#/HackerRank Easy Problems/Counting Valleys/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        char upStep = 'U';
+        int numValleys = 0;
+        int currentAlt = 0;
+
+        for (int stepIndex = 0; stepIndex < steps; stepIndex++)
+        {
+            if (path[stepIndex] == upStep)
+            {
+                currentAlt++;
+                if (currentAlt == 0)
+                {
+                    numValleys++;
+                }
+            }
+            else
+            {
+                currentAlt--;
+            }
+        }
+
+        return numValleys;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int steps = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string path = Console.ReadLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
